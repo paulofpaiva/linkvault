9e6aaf8aa1e4f1afea5abd86bf81f07ac6098fd1
paulofpaiva <paivank2001@gmail.com>
2025-10-29 18:30:55 -0300

    revert fix build 1

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,9 +1,9 @@
 // Schemas
-export * from './schemas/auth.schema';
-export * from './schemas/links.schema';
-export * from './schemas/categories.schema';
+export * from './schemas/auth.schema.js';
+export * from './schemas/links.schema.js';
+export * from './schemas/categories.schema.js';
 
 // Types
-export * from './types/auth';
-export * from './types/links';
-export * from './types/categories';
+export * from './types/auth.js';
+export * from './types/links.js';
+export * from './types/categories.js';
